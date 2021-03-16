7516b12a51590bbab71d0ccae74ae55e941fe081
Michael Meffie <mmeffie@sinenomine.net>
2021-03-16 13:09:16 -0400

    tests: Run molecule in separate process
    When running the unit tests, run molecule in a separate process
    so it has a fresh state for each test.

--- tests/test_command.py
+++ tests/test_command.py
@@ -7,7 +7,7 @@ from contextlib import contextmanager
 from click.testing import CliRunner
 import pytest
 
-from molecule.shell import main
+from molecule.util import run_command
 
 @contextmanager
 def change_dir(path):
@@ -20,15 +20,15 @@ def change_dir(path):
 
 def test_molecule_init():
     """Verify that init scenario works."""
-    options = ['init', 'role', 'test-init', '--verifier-name', 'robotframework']
+    cmd = ['molecule', 'init', 'role', 'test-init', '--verifier-name', 'robotframework']
     runner = CliRunner()
     with runner.isolated_filesystem():
-        result = runner.invoke(main, options)
-        assert result.exit_code == 0
+        result = run_command(cmd)
+        assert result.returncode == 0
         verify_yml = Path('./test-init/molecule/default/verify.yml')
         assert verify_yml.exists()
         assert verify_yml.is_file()
-        assert 'Install Robot Framework' in verify_yml.read_text()
+        assert 'robotframework' in verify_yml.read_text()
 
 @pytest.mark.parametrize(
     ('scenario_to_test', 'image'),
@@ -42,14 +42,13 @@ def test_molecule_init():
     ]
 )
 def test_molecule_scenario(scenario_to_test, image):
-    runner = CliRunner()
-    image_name = image.replace('/', '-').replace(':', '-')
-    tmp = Path('/tmp/scenario-%s-%s.txt' % (scenario_to_test, image_name))
+    #image_name = image.replace('/', '-').replace(':', '-')
+    #tmp = Path('/tmp/scenario-%s-%s.txt' % (scenario_to_test, image_name))
     basedir = Path(__file__).resolve().parent
     testdir = basedir / 'scenarios' / scenario_to_test
     with change_dir(testdir):
         env = yaml.dump({'IMAGE': image})
         Path('.env.yml').write_text(env)
-        result = runner.invoke(main, ['test'])
-        tmp.write_text(result.output)
-        assert result.exit_code == 0
+        result = run_command(['molecule', 'test'])
+        #tmp.write_text(result.output)
+        assert result.returncode == 0
