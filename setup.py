b900e2be38790a795e3ad4069d24b063e261009b
Michael Meffie <mmeffie@sinenomine.net>
2022-06-17 09:28:48 -0400

    Limit molecule version to 3.x
    Molecule 4.0.0 makes some breaking changes, so limit our version
    to 3.x until we catch up.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
     },
     install_requires=[
         # molecule plugins are not allowed to mention Ansible as a direct dependency
-        'molecule>=3.2.0',
+        'molecule>=3.2.0,<4',
         'pyyaml>=5.1,<6',
     ],
     classifiers=[
